08fc2cdfcf6090a8705a534ab662d752f4fd4791
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2019-09-17 21:28:43 +0530

    add method: toString() add constructor: Book()

--- src/main/Book.java
+++ src/main/Book.java
@@ -1,5 +1,45 @@
 package main;
 
 class Book {
+    String bookName;
+    String authorName;
+    String isbn;
 
+    public Book(String bookName, String authorName, String isbn) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.isbn = isbn;
+
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String b) {
+        bookName = b;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String a) {
+        authorName = a;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String i) {
+        isbn = i;
+    }
+
+    public String toString() {
+        return "-----------------------------" + "\n" +
+                "Author Name:\t" + getAuthorName() + "\n" +
+                "ISBN:\t" + getIsbn() + "\n" +
+                "-----------------------------";
+    }
 }
